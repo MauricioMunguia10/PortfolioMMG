8034d7139d10d2278dff87e94fc2c91f2abeed4e
MauricioMunguia10 <mauriciomunguiagarcia@gmail.com>
2024-09-27 14:42:26 -0600

    page portfolio
    add design

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,9 +1,18 @@
+import NavBar from "@/components/navBar/navBar";
+import styles from "./portfolio.module.css";
+import Card from "@/components/card/card";
 const Portfolio = () => {
   return (
-    <div>
-      Enter
+    <div className={styles.container}>
+      <NavBar />
+      <div className={styles.title}>Projects</div>
+      <div className={styles.cards}>
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </div>
   );
-}
+};
 
 export default Portfolio;
